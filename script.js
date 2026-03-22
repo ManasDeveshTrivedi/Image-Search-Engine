af9e2344b2adcc2215d8521b4c88fd32ce06b26f
Manas <manasdevtrivedi@gmail.com>
2026-03-22 21:20:30 +0530

    Added

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ let page=1;
 
 async function searchImages(){
     keyword=searchBox.value;
-    const url=`https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}`;
+    const url=`https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
     const response=await fetch(url);
     const data=await response.json();
 
@@ -26,12 +26,18 @@ async function searchImages(){
         imageLink.appendChild(image);
         searchResult.appendChild(imageLink);
     })
-
+    searchMoreBtn.style.display="block";
 }
 
 
 searchForm.addEventListener("submit",(e)=>{
     e.preventDefault();
     page=1;
+    searchImages();
+})
+
+
+searchMoreBtn.addEventListener("click",()=>{
+    page++;
     searchImages();
 })
