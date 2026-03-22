a20269a1113c68d22ef1c1fadbdfcb5640857c46
Manas <manasdevtrivedi@gmail.com>
2026-03-22 14:43:54 +0530

    Added

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
+const accessKey="HL0Hhz9ucoty-V4JQ32jB6RlCFL4vaQ3T-W8W7X3Imc";
+
 const searchForm=document.getElementById("search-form");
 const searchBox=document.getElementById("search-box");
 const searchResult=document.getElementById("search-result");
 const searchMoreBtn=document.getElementById("show-more-btn");
+
+
+let keyword="";
+let page=1;
+
+async function searchImages(){
+    keyword=searchBox.value;
+    const url=`https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}`;
+    const response=await fetch(url);
+    const data=await response.json();
+    console.log(data);
+    
+}
